fa76f7314544db8e81e0fb0459c04c6fc54d9431
Jonathan Fine <jfine@pytex.org>
2015-07-19 13:18:34 +0100

    Add by_score handlers.

--- talks.js
+++ talks.js
@@ -26,14 +26,31 @@ var star_handler = function(event){
 };
 
 
+var by_score_handler = function(event){
+
+    var by_score = up_to_class(event.target, 'by-score');
+    jQuery('.talk').hide();
+    jQuery('.talk.score-' + event.data).show();
+};
+
+
+var by_score_all_handler = function(event){
+
+    var by_score = up_to_class(event.target, 'by-score');
+    jQuery('.talk').show();
+};
+
+
 var ready_handler = function()
 {
     var i;
     for(i = 0; i <= 3; i++)
     {
         jQuery('.star-' + i).on('click', i, star_handler);
+        jQuery('.by-score-' + i).on('click', i, by_score_handler);
     }
 
+    jQuery('.by-score-all').on('click', by_score_all_handler);
 };
 
 
