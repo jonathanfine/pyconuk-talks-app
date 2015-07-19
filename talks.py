6a7f4e7b5750b3507f48a0594a16dd3a23a43673
Jonathan Fine <jfine@pytex.org>
2015-07-19 12:45:39 +0100

    Oops - fix class typo.

--- talks.py
+++ talks.py
@@ -13,18 +13,18 @@ TEMPLATE = '''\
 <p class='by-score'>
 Score:
 <span class='by-score-all'>All</span>
-<span 'by-score-0'>0</span>
-<span 'by-score-1'>1</span>
-<span 'by-score-2'>2</span>
-<span 'by-score-3'>3</span>
+<span class='by-score-0'>0</span>
+<span class='by-score-1'>1</span>
+<span class='by-score-2'>2</span>
+<span class='by-score-3'>3</span>
 </p>
 
 <p class='by-category'>
 Category:
 <span class='by-category-all'>All</span>
-<span 'by-category-1'>One</span>
-<span 'by-category-2'>Two</span>
-<span 'by-category-3'>Three</span>
+<span class='by-category-1'>One</span>
+<span class='by-category-2'>Two</span>
+<span class='by-category-3'>Three</span>
 </p>
 </div>
 
