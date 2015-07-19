b8e98a6996c643159af3dcc98831d23f49ab8f33
Jonathan Fine <jfine@pytex.org>
2015-07-19 13:00:43 +0100

    Refactor to create up_to_class(elt, cls).

--- talks.js
+++ talks.js
@@ -1,22 +1,25 @@
 // alert('hi');
 var jQuery;
 
-var star_handler = function(event){
 
-    //    alert(event.data);
-    var star = event.target;
-    var tmp = star;
-    var talk = undefined;
-    while(!talk) {
-        if (jQuery(tmp).hasClass('talk'))
+var up_to_class = function(elt, cls)
+{
+    var candidate = elt;
+    while(elt){
+        if (jQuery(elt).hasClass(cls))
         {
-            talk = tmp;
-            break;
+            return elt;
         }
-        // Break if we get to the body element.
-        tmp = tmp.parentNode;
+        // document.parentNode == null.
+        elt = elt.parentNode;
     }
+    return null;
+};
 
+
+var star_handler = function(event){
+    //    alert(event.data);
+    var talk = up_to_class(event.target, 'talk');
     jQuery(talk).removeClass('score-0 score-1 score-2 score-3');
     jQuery(talk).addClass('score-' + event.data);
     //    alert(talk);
